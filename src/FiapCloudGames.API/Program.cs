9d0440a34cf82e0e59db4b330fddec8b14d866f1
Lucas Nunes <lucas.nunes@miketec.com.br>
2026-01-15 01:16:55 -0300

    Ajustes e merge com a branch do Oberdan.

--- src/FiapCloudGames.API/Program.cs
+++ src/FiapCloudGames.API/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddSwaggerGen(options =>
         Description =
             "Plataforma de venda de jogos digitais e gest�o de servidores para partidas online \n\n" +
             "Contatos:\n\n" +
-            "- Joao Paulo\n\n" +
+            "- Joao Paulo RM 370112\n\n" +
             "- Lucas Nunes RM 369391\n\n" +
             "- Marcos Antonio RM 368502\n\n" +
             "- David RM 369381\n\n" +
@@ -60,11 +60,9 @@ builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IGamesRepository, GamesRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAcessUserRepository, AcessUserRepository>();
 
 builder.Services.AddScoped<IAcessUserService, AcessUserService>();
-builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddScoped<IAuthenticateService, AuthenticateService>();
